eea1a48485db02cb15874d67ecd98c9f565dd342
IKROMJON <ikromjon.sharip@gmail.com>
2024-04-26 11:46:07 +0500

    some changes

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+const PORT = process.env.PORT || 4100
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+
+
+
+
+
+
+
+
+
