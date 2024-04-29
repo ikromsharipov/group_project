b0a6ab6239a5f6951df69d3fe7f11faefdacef83
IKROMJON <ikromjon.sharip@gmail.com>
2024-04-29 13:57:06 +0500

    created html file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import express from 'express'
+import express from 'express';
+import path from 'path';
 
 const app = express()
 
-const PORT = process.env.PORT || 4100
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+    res.send('Main page')
+})
 
+const PORT = process.env.PORT || 4100
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
 
 
