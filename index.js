63c9bc1dc01c7d97ad5bfbd1d205a5c5402da1a3
IKROMJON <ikromjon.sharip@gmail.com>
2024-05-10 20:29:40 +0500

    Layout

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import express from 'express';
-import path from 'path';
+import {create} from 'express-handlebars'
 
 const app = express()
 
+const hbs = create({defaultLayout: 'main', extname: 'hbs'})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"))
-    res.send('Main page')
-})
+    res.render('index')
+    // res.sendFile(path.join(__dirname, "views", "index.html"))
+    // res.send('Main ')
+});
+
+app.get('/about', (req, res) => {
+    res.render('about')
+    // res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
 
 const PORT = process.env.PORT || 4100
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
