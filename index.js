33c0258167f2c487f64645dd76d71aa3fbbc0124
IKROMJON <ikromjon.sharip@gmail.com>
2024-05-11 20:03:09 +0500

    some changes

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import {create} from 'express-handlebars';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv'
+
+// Routes
 import AuthRoutes from './routes/auth.js';
 import CoursesRoutes from './routes/courses.js';
+
+dotenv.config()
 
 const app = express()
 
@@ -12,16 +18,37 @@ app.set('view engine', 'hbs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 
 app.use(AuthRoutes);
 app.use(CoursesRoutes);
 
+// console.log(process.env.MONGO_URI);
+// Set strictQuery to false
+mongoose.set('strictQuery', false);
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB connected");
+        // Additional initialization code can go here
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
+
+/*
+{
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+},
+ */
 
 const PORT = process.env.PORT || 4100
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
 
-
+// mongodb+srv://ikromjonsharip:<password>@ikromjon.0jhmppb.mongodb.net/?retryWrites=true&w=majority&appName=ikromjon
 
 
 
